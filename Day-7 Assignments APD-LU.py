195d439fc9f8cb596459d382b6d21ab5cde7eadd
TarunKalva <70630420+TarunKalva@users.noreply.github.com>
2020-10-11 20:51:54 +0530

    Day-7 Assignment APD-LU

---
+++ Day-7 Assignments APD-LU.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Assignment
+# Given two lists merge the lists and print the sorted lists without using sort or sorted methods
+
+# In[19]:
+
+
+list1=[10,20,30,40,50]
+list2=[5,15,25,35,45,60]
+list3=list1+list2
+sorted_list=[]
+while list3:
+    mini=list3[0]
+    for item in list3:
+        if item<mini:
+            mini=item
+    sorted_list.append(mini)
+    list3.remove(mini)
+print(sorted_list)    
+
+
+# In[ ]:
+
+
+
+
